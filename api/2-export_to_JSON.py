7dee2e5c228b7a48f37570a06298835f96d3893c
iShezmu <arteaga25610@gmail.com>
2024-01-21 09:32:06 -0800

    new: extend python script to export data in the JSON format.

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Script that exports data in JSON format for a given employee ID.
+
+Usage: ./export_to_json.py <employee id>
+"""
+
+import json
+import requests
+import sys
+
+API_URL = "https://jsonplaceholder.typicode.com/"
+
+def export_to_json(employee_id):
+    """
+    Export data in JSON format for a given employee ID.
+
+    Args:
+        employee_id (int): The employee ID.
+
+    Returns:
+        None
+    """
+    employee = requests.get(API_URL + "users/{}".format(employee_id)).json()
+
+    todo_list = requests.get("{}todos?userId={}".format(API_URL,
+                                                        employee_id)).json()
+
+    with open("{}.json".format(employee_id), "w") as json_file:
+        json.dump({employee_id: [{
+            "task": task.get("title", ""),
+            "completed": task.get("completed", ""),
+            "username": employee.get("username", "")
+        } for task in todo_list]}, json_file, indent=2)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./export_to_json.py <employee id>")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+    export_to_json(int(employee_id))
+
--- api/README.md
+++ api/README.md
@@ -18,3 +18,10 @@
         - Records all tasks that are owned by this employee
         - Format must be: `"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"`
         - File name must be: `USER_ID.csv`
+
+*   2-export_to_JSON.py
+    - Using what you did in the task #0, extend your Python script to export data in the JSON format.
+      - Requirements:
+        - Records all tasks that are owned by this employee
+        - Format must be: `{ "USER_ID": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}`
+        - File name must be: `USER_ID.json`
